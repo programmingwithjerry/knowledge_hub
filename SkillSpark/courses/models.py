531814b528033d384b87ba55b8d671e267c7fde2
programmingwithjerry <eneyellowj@gmail.com>
2025-01-09 20:06:38 +0100

    Created models for polymorphic contents

--- SkillSpark/courses/models.py
+++ SkillSpark/courses/models.py
@@ -1,3 +1,5 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -73,7 +75,7 @@ class Module(models.Model):
     course = models.ForeignKey(
         Course,
         related_name='modules',
-        on_delete=models.CASCADE  # Delete module if the course is deleted.
+        on_delete=models.CASCADE #Delete module if the course is deleted.
     )
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
@@ -85,3 +87,119 @@ class Module(models.Model):
             str: The title of the module.
         """
         return self.title
+
+
+class Content(models.Model):
+    """
+    Model representing a content item within a module.
+
+    This model uses Django's ContentTypes framework to allow
+    flexible associations with any model, enabling the creation
+    of reusable and dynamic content blocks.
+
+    Attributes:
+        module (Module): The module this content item belongs to.
+        content_type (ContentType): The type of content
+        (e.g., text, video, file).
+        object_id (int): The ID of the related content object.
+        item (GenericForeignKey): A generic foreign key combining
+        content_type and object_id.
+    """
+    module = models.ForeignKey(
+        Module,
+        related_name='contents',
+        on_delete=models.CASCADE
+    )
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to={
+            'model__in':('text', 'video', 'image', 'file')
+        }
+    )
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+    def __str__(self) -> str:
+        """
+        String representation of a Content object.
+        Return:string indicating the module and the associated content item.
+        """
+        return f"Content for module {self.module} - {self.item}"
+
+
+class ItemBase(models.Model):
+    """
+    Abstract base model for reusable content types.
+
+    Attributes:
+        owner (User): The user who created the item.
+        title (str): The title of the item.
+        created (datetime): The date and time the item was created.
+        updated (datetime): The date and time the item was last updated.
+    """
+    owner = models.ForeignKey(
+        User,
+        related_name='%(class)s_related',
+        on_delete=models.CASCADE  # Delete the item if the owner is deleted.
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        Meta class for the ItemBase model.
+
+        Specifies that this is an abstract model,
+        so it will not create a database table.
+        """
+        abstract = True
+
+    def __str__(self) -> str:
+        """
+        String representation of an ItemBase object.
+        Return:
+            str: The title of the item.
+        """
+        return self.title
+
+
+class Text(ItemBase):
+    """
+    Model representing a text-based content item.
+
+    Attributes:
+        content (str): The textual content.
+    """
+    content = models.TextField()
+
+
+class File(ItemBase):
+    """
+    Model representing a file-based content item.
+
+    Attributes:
+        file (File): The uploaded file.
+    """
+    file = models.FileField(upload_to='files')
+
+
+class Image(ItemBase):
+    """
+    Model representing an image-based content item.
+
+    Attributes:
+        file (File): The uploaded image file.
+    """
+    file = models.FileField(upload_to='images')
+
+
+class Video(ItemBase):
+    """
+    Model representing a video-based content item.
+
+    Attributes:
+        url (str): The URL of the video.
+    """
+    url = models.URLField()  # Stores the URL of the video.
